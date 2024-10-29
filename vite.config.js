27a59820c1350d931c8aaecbeb8c1c39ff80b11f
MAST CODER <officialmastcoder@gmail.com>
2024-10-30 01:26:44 +0530

    try

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // Adjust this if your project is hosted in a subdirectory
+  base: '/',
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
