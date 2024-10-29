970235f7592d98b93485eb8a9ec92a83b6aa3e61
MAST CODER <officialmastcoder@gmail.com>
2024-10-30 00:45:10 +0530

    fix: vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  //Added These Lines for client side routing
   server: {
     historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
